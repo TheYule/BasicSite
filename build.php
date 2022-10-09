82ec79ad34f5f93adc258a77b627c35b30d2dda9
The Yule <xander@yoo-babobo.com>
2022-10-09 13:21:44 -0400

    Updated PHP build file

--- build.php
+++ build.php
@@ -1,16 +1,59 @@
 <?php
-    $files = glob("src/*.php");
+    function items($dir, &$results = array()) {
+        $files = scandir($dir);
+
+        foreach ($files as $key => $value) {
+            $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+            if (!is_dir($path)) $results[] = ["type" => "file", "path" => $path];
+            else if ($value != "." && $value != "..") {
+                $results[] = ["type" => "folder", "path" => $path];
+                items($path, $results);
+            }
+        }
+    
+        return $results;
+    }
+
+    function deleteAll($dir) {
+        foreach(glob($dir . "/*") as $file) {
+            if (is_dir($file)) deleteAll($file);
+            else unlink($file);
+        }
+        rmdir($dir);
+    }
+
+    $items = items("src");
+
+    deleteAll("dist");
+    mkdir("dist");
 
     echo "Building...\n\n";
 
-    foreach ($files as $file) {
-        $output = str_replace("src/", "dist/", $file);
-        $output = str_replace(".php", ".html", $output);
+    foreach ($items as $item) {
+        $type = $item["type"];
+        $path = $item["path"];
+        $new = str_replace("src", "dist", $path);
+        
+        if ($type == "file") {
+            if (preg_match("/\.php$/", $new)) {
+                $new = str_replace(".php", ".html", $new);
 
-        exec("php $file > $output");
+                echo "Building file $new...\n";
+    
+                exec("php $path > $new");
+            } else {
+                $contents = file_get_contents($path);
 
-        echo "Building $file...\n\n";
+                echo "Creating file $new...\n";
+    
+                file_put_contents($new, $contents);
+            }
+        } else if ($type == "folder") {
+            echo "Creating folder $new...\n";
+
+            mkdir($new);
+        }
     }
 
-    echo "Build complete.";
+    echo "\nBuild complete.";
 ?>
